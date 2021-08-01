4de2038186e7f4d57a961eb8fa18c19c146bf4be
Andrew Imm <aimm22@gmail.com>
2021-08-01 16:28:55 -0700

    Add instruction bytes to disassembled instructions

--- src/debug/disassembly/mod.rs
+++ src/debug/disassembly/mod.rs
@@ -1,23 +1,43 @@
-use std::fmt::format;
-
 use crate::decoder;
 
-pub struct Instruction(pub u16, pub String);
+pub struct Instruction {
+  address: u16,
+  bytes: [u8; 4],
+  length: usize,
+  text: String,
+}
 
 impl std::fmt::Display for Instruction {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    f.write_fmt(format_args!("{:#06X}  {}", self.0, self.1))
+    let mut formatted = format!("{:#06X}  ", self.address);
+    for i in 0..self.length {
+      formatted.push_str(format!("{:02X} ", self.bytes[i]).as_str());
+    }
+    for _ in self.length..4 {
+      formatted.push_str("   ");
+    }
+    formatted.push_str(self.text.as_str());
+    f.write_str(formatted.as_str())
   }
 }
 
 pub fn disassemble(initial_addr: u16, instructions: &[u8]) -> Vec<Instruction> {
   let mut output = Vec::new();
   let mut cursor = 0;
-  let mut addr = initial_addr;
+  let mut address = initial_addr;
   while cursor < instructions.len() {
     let (op, length, _) = decoder::decode(&instructions[cursor..]);
-    output.push(Instruction(addr, format(format_args!("{}", op))));
-    addr = addr.wrapping_add(length as u16);
+    let mut bytes: [u8; 4] = [0; 4];
+    for i in 0..length {
+      bytes[i] = instructions[cursor + i];
+    }
+    output.push(Instruction {
+      address,
+      bytes,
+      length,
+      text: op.to_string(),
+    });
+    address = address.wrapping_add(length as u16);
     cursor += length;
   }
 
