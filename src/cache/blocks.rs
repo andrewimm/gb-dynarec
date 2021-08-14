1fa0baada9dec52513721b1af01a705dfbb3d958
Andrew Imm <aimm22@gmail.com>
2021-08-14 12:45:18 -0700

    Break the JIT cache into individual regions, which should make banking support easier

---
+++ src/cache/blocks.rs
@@ -0,0 +1,151 @@
+use std::collections::BTreeMap;
+
+/// CodeBlock is a simple pointer to a region of JIT executable memory that
+/// contains translated code
+pub struct CodeBlock {
+  pub offset: usize,
+  pub length: usize,
+}
+
+pub struct MemoryLocation {
+  bank: u16,
+  address: u16,
+}
+
+impl MemoryLocation {
+  pub fn new(bank: u16, address: u16) -> Self {
+    Self {
+      bank,
+      address,
+    }
+  }
+
+  pub fn as_u32(&self) -> u32 {
+    ((self.bank as u32) << 16)
+      | (self.address as u32)
+  }
+
+  pub fn from_u32(value: u32) -> Self {
+    let bank = (value >> 16) as u16;
+    let address = value as u16;
+    Self {
+      bank,
+      address,
+    }
+  }
+}
+
+/// A CacheRegion is a smaller cache for a specific segment of the GB memory
+/// map. Because of banking, a single address on the GB bus can point to
+/// different blocks of code at different times.
+/// Creating a separate cache for each banked region of memory makes it easier
+/// to perform lookup for a specific GB address.
+pub struct CacheRegion {
+  cache: BTreeMap<u32, CodeBlock>,
+  current_bank: u16,
+}
+
+impl CacheRegion {
+  pub fn new(current_bank: u16) -> Self {
+    Self {
+      cache: BTreeMap::new(),
+      current_bank,
+    }
+  }
+
+  pub fn insert(&mut self, address: u16, block: CodeBlock) {
+    let location = MemoryLocation::new(self.current_bank, address);
+    let key = location.as_u32();
+    self.cache.insert(key, block);
+  }
+
+  pub fn get(&self, address: u16) -> Option<&CodeBlock> {
+    let location = MemoryLocation::new(self.current_bank, address);
+    let key = location.as_u32();
+    self.cache.get(&key)
+  }
+
+  pub fn set_bank(&mut self, bank: u16) {
+    self.current_bank = bank;
+  }
+}
+
+/// CachedBlocks stores individual lookup caches for each region of memory that
+/// could reasonably store code.
+pub struct CachedBlocks {
+  rom_low: CacheRegion,   // 0x0000 - 0x3fff
+  rom_high: CacheRegion,  // 0x3fff - 0x7fff
+  cart_ram: CacheRegion,  // 0xa000 - 0xbfff
+  wram_low: CacheRegion,  // 0xc000 - 0xcfff
+  wram_high: CacheRegion, // 0xd000 - 0xdfff
+  high_ram: CacheRegion,  // 0xff80 - 0xfffe
+}
+
+impl CachedBlocks {
+  pub fn new() -> Self {
+    Self {
+      rom_low: CacheRegion::new(0),
+      rom_high: CacheRegion::new(1),
+      cart_ram: CacheRegion::new(0),
+      wram_low: CacheRegion::new(0),
+      wram_high: CacheRegion::new(1),
+      high_ram: CacheRegion::new(0),
+    }
+  }
+
+  pub fn get_region(&self, addr: u16) -> Option<&CacheRegion> {
+    if addr < 0x4000 {
+      return Some(&self.rom_low);
+    }
+    if addr < 0x8000 {
+      return Some(&self.rom_high);
+    }
+    if addr < 0xa000 {
+      return None;
+    }
+    if addr < 0xc000 {
+      return Some(&self.cart_ram);
+    }
+    if addr < 0xd000 {
+      return Some(&self.wram_low);
+    }
+    if addr < 0xe000 {
+      return Some(&self.wram_high);
+    }
+    if addr < 0xff80 {
+      return None;
+    }
+    if addr != 0xffff {
+      return Some(&self.high_ram);
+    }
+    None
+  }
+
+  pub fn get_region_mut(&mut self, addr: u16) -> Option<&mut CacheRegion> {
+    if addr < 0x4000 {
+      return Some(&mut self.rom_low);
+    }
+    if addr < 0x8000 {
+      return Some(&mut self.rom_high);
+    }
+    if addr < 0xa000 {
+      return None;
+    }
+    if addr < 0xc000 {
+      return Some(&mut self.cart_ram);
+    }
+    if addr < 0xd000 {
+      return Some(&mut self.wram_low);
+    }
+    if addr < 0xe000 {
+      return Some(&mut self.wram_high);
+    }
+    if addr < 0xff80 {
+      return None;
+    }
+    if addr != 0xffff {
+      return Some(&mut self.high_ram);
+    }
+    None
+  }
+}
--- src/cache/mod.rs
+++ src/cache/mod.rs
@@ -1,14 +1,14 @@
-
+pub mod blocks;
 #[cfg(unix)]
 pub mod linux;
 #[cfg(windows)]
 pub mod windows;
 
+use blocks::{CachedBlocks, CodeBlock};
 use crate::cpu::Registers;
 use crate::decoder::decode;
 use crate::emitter::Emitter;
 use crate::mem::MemoryAreas;
-use std::collections::BTreeMap;
 
 #[cfg(unix)]
 use linux::ExecutableMemory;
@@ -19,15 +19,9 @@ pub const INITIAL_MEMORY_SIZE: usize = 0x10000;
 pub const MEMORY_MINIMUM_SIZE: usize = 0x1000;
 pub const MEMORY_SIZE_INCREASE: usize = 0x1000;
 
-pub struct CodeBlock {
-  pub offset: usize,
-  pub length: usize,
-}
-
 pub struct CodeCache {
   exec_memory: ExecutableMemory,
-  code_blocks: BTreeMap<usize, CodeBlock>,
-  jump_cache: BTreeMap<usize, usize>,
+  code_blocks: CachedBlocks,
   write_cursor: usize,
 }
 
@@ -35,8 +29,7 @@ impl CodeCache {
   pub fn new() -> Self {
     Self {
       exec_memory: ExecutableMemory::new(),
-      code_blocks: BTreeMap::new(),
-      jump_cache: BTreeMap::new(),
+      code_blocks: CachedBlocks::new(),
       write_cursor: 0,
     }
   }
@@ -46,9 +39,29 @@ impl CodeCache {
   }
 
   pub fn get_address_for_ip(&self, ip: usize) -> Option<usize> {
-    match self.code_blocks.get(&ip) {
-      Some(block) => return Some(block.offset),
-      None => self.jump_cache.get(&ip).copied(),
+    let gb_ip = ip as u16;
+    self.code_blocks
+      .get_region(gb_ip)
+      .and_then(|region| region.get(gb_ip))
+      .and_then(|block| Some(block.offset))
+  }
+
+  pub fn get_executable_memory_segment(&self, ip: usize, mem_ptr: *const MemoryAreas) -> &[u8] {
+    let mem = unsafe { &*mem_ptr };
+    match ip {
+      0x0000..=0x3fff => &mem.rom[ip..],
+      0x4000..=0x7fff => {
+        let mut offset = ip & 0x3fff;
+        offset += mem.rom_bank * 0x4000;
+        &mem.rom[offset..]
+      },
+      0xc000..=0xcfff => &mem.work_ram[(ip & 0xfff)..],
+      0xd000..=0xdfff => {
+        let mut offset = ip & 0xfff;
+        offset += mem.wram_bank * 0x1000;
+        &mem.work_ram[offset..]
+      },
+      _ => panic!(),
     }
   }
 
@@ -56,30 +69,25 @@ impl CodeCache {
     let mut write_cursor = self.write_cursor;
     let starting_offset = write_cursor;
 
-    self.exec_memory.make_writable();
-    let translated = self.exec_memory.get_memory_area_mut();
-    let available_length = translated.len();
     let emitter = Emitter::new(mem);
-    write_cursor += emitter.encode_prelude(&mut translated[write_cursor..]);
+    let available_length = {
+      self.exec_memory.make_writable();
+      let translated = self.exec_memory.get_memory_area_mut();
+      write_cursor += emitter.encode_prelude(&mut translated[write_cursor..]);
+      translated.len()
+    };
 
     let mut block_ended = false;
     let mut index = ip;
     while !block_ended {
-      // TODO: needs to handle MBC banking
-      let code_slice = match index {
-        0..=0x7fff => &code[index..],
-        0xa000..=0xbfff => panic!("Executing external RAM"),
-        0xc000..=0xdfff => unsafe {
-          &(*mem).work_ram[(index - 0xc000)..]
-        },
-        _ => panic!(),
-      };
+      let code_slice = self.get_executable_memory_segment(index, mem);
       if code_slice.len() < 1 {
         break;
       }
       let (next_op, length, _cycles) = decode(code_slice);
       index += length;
       block_ended = next_op.is_block_end();
+      let translated = self.exec_memory.get_memory_area_mut();
       let written = emitter.encode_op(next_op, length, &mut translated[write_cursor..]);
       write_cursor += written;
     }
@@ -101,8 +109,10 @@ impl CodeCache {
       println!("  ==  ");
     }
     
-
-    write_cursor += emitter.encode_epilogue(&mut translated[write_cursor..]);
+    {
+      let translated = self.exec_memory.get_memory_area_mut();
+      write_cursor += emitter.encode_epilogue(&mut translated[write_cursor..]);
+    }
 
     self.exec_memory.make_executable();
     self.write_cursor = write_cursor;
@@ -118,8 +128,11 @@ impl CodeCache {
   }
 
   fn insert_code_block(&mut self, ip: usize, offset: usize, length: usize) {
-    self.code_blocks.insert(
-      ip,
+    let region = self.code_blocks
+      .get_region_mut(ip as u16)
+      .expect("Cannot cache code in this region");
+    region.insert(
+      ip as u16,
       CodeBlock {
         offset,
         length,
